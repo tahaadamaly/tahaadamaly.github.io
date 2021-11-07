68d5ed9dcf1c3375a3934146f7f262c506131c59
tahaadamaly <taha@adamaly.com>
2021-11-07 13:42:43 +0000

    Updates

---
+++ apifetch.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import time
+
+apibase = "https://api.nomics.com/v1/exchange-rates/history?key=09a184e1807679b00d552874f6257bf2c615a327&currency={}&start=2010-04-14T00%3A00%3A00Z&end=2021-09-14T00%3A00%3A00Z"
+coinlist = ['BTC', 'ETH', 'USDT', 'SOL']
+fileBase = "crypto_{}_history.json"
+
+
+for i in coinlist:
+    apiurl = apibase.format(i)
+    data = requests.get(apiurl).json()
+    fileName = fileBase.format(i)
+    with open(fileName, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    time.sleep(1)
+
+
+
+
--- main.py
+++ main.py
@@ -22,14 +22,35 @@ def get_constructor_pos(year):
     pos = df[['position', 'name', 'points']]
     return pos
 
+def get_winning_constructor(year):
+    df = f1.constructor_standings(year, None)
+    winner = df.loc[df['position'] == str(1), 'name'].iloc[0]
+    return winner
+
+def get_winning_driver(year):
+    df = f1.driver_standings(year, None)
+    winner = df.loc[df['position'] == str(1), 'driver'].iloc[0]
+    return winner
+
 def get_driver_standings(year):
     df = f1.driver_standings(year, None)   
     pos = df[['position', 'driver', 'points']]
     return pos
 
-
+"""
 req = input("Driver or Constructor Standings? (D/C): ")
 if req=='D':
     pprint_df(get_driver_standings(int(input('Enter year: '))))
 elif req=='C':
     pprint_df(get_constructor_pos(int(input('Enter year: '))))
+"""
+
+winners = []
+
+for i in range(1960, 2020):
+    winners.append((i, get_winning_constructor(i), get_winning_driver(i)))
+
+for i in winners:
+    print(i)
+
+
