15b889f517b0d76c733e8c0b51fc0cbf7b2c5c60
tahaadamaly <taha@adamaly.com>
2021-11-17 17:59:01 +0000

    Data Added

---
+++ apifetch copy.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import time
+
+apibase = "https://api.stlouisfed.org/fred/series/observations?series_id={}&api_key=2677c7ef979aaca29feb223566280265&file_type=json&observation_start=2019-08-01&observation_end=2021-08-01"
+serieslist = ['SWEPFCEQDSMEI']
+fileBase = "fred_consump_{}.json"
+
+# API Fetch for 4 different cryptocurrencies from the Nomics API
+
+for i in serieslist:
+    apiurl = apibase.format(i)
+    data = requests.get(apiurl).json()
+    fileName = fileBase.format(i)
+    with open('data/{}'.format(fileName), 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    # Time delay is added because the API doesn't seem to be able to handle 4 rapid requests, as an error occurs unless this delay is added
+    time.sleep(1)
+
+
+
+
