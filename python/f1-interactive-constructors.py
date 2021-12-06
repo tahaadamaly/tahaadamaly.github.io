b2df5c227c85b7a580bf290f90da37d46952a032
tahaadamaly <taha@adamaly.com>
2021-12-06 20:47:25 +0000

    Update

--- python/f1-interactive-constructors.py
+++ python/f1-interactive-constructors.py
@@ -8,20 +8,18 @@ race_country = ['Bahrain', 'Italy', 'Portugal', 'Spain', 'Monaco', 'Azerbaijan',
 'Russia', 'Turkey', 'United States', 'Mexico', 'Brazil', 'Qatar', 'Saudi Arabia']
 
 
-for i in range(1, 22):
-    df = f1.driver_standings(2021, i)
-    df = df[['driver', 'points', 'driverID']]
-    df['race'] = i
-    df['race_country'] = race_country[i-1]
+for i in range(2000, 2022):
+    df = f1.constructor_standings(2021)
+    df = df[['name', 'points', 'constructorID']]
+    df['year'] = i
     master_data = master_data.append(df)
 
-# Delete any drivers not in the top 10 as of round 21
-for x in master_data['driverID']:
-    if x not in top_ten_drivers:
-        master_data.drop(master_data.index[master_data['driverID'] == x], inplace=True)
+
+print(master_data)
+
+master_data.to_csv('data/f1_constr_points_yearly.csv')
 
 
-#master_data.to_csv('data/f1_2021_top_driver_points.csv')
 
 
 
