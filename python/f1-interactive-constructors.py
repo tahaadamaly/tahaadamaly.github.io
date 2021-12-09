08bf63ae4ae7c2bd40d6beb25907b10f7982ee4f
tahaadamaly <taha@adamaly.com>
2021-12-09 15:39:45 +0000

    Update

--- python/f1-interactive-constructors.py
+++ python/f1-interactive-constructors.py
@@ -3,11 +3,6 @@ import formula1 as f1
 
 master_data = pd.DataFrame()
 
-top_ten_drivers = ['max_verstappen', 'hamilton', 'bottas', 'perez', 'leclerc', 'norris', 'sainz', 'ricciardo', 'alonso', 'gasly']
-race_country = ['Bahrain', 'Italy', 'Portugal', 'Spain', 'Monaco', 'Azerbaijan', 'France', 'Austria', 'Austria', 'Great Britain', 'Hungary', 'Belgium', 'Netherlands', 'Italy',
-'Russia', 'Turkey', 'United States', 'Mexico', 'Brazil', 'Qatar', 'Saudi Arabia']
-
-
 for i in range(2000, 2022):
     df = f1.constructor_standings(i)
     df = df[['name', 'points', 'constructorID']]
